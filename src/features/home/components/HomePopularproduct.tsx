5f2ad4de832ce792a5cb050a2baa9a15d3fcff7d
MuhammadNiazAli <mrniazali132@gmail.com>
2026-01-16 15:35:32 +0500

    all is perfectly cleard

--- src/features/home/components/HomePopularproduct.tsx
+++ src/features/home/components/HomePopularproduct.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import ShopCover from "../../shop/ShopCover";
 
 const HomePopularproduct = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="w-full">
+      <h2 className="text-[22px] font-medium text-gray-700 px-25 mt-10">
+         Popular{" "}Pro
+        <span className="border-b-2 border-orange-600">
+          ducts
+        </span>
+      </h2>
 
-export default HomePopularproduct
+      <ShopCover showTitle={false} />
+      <div className="w-full h-20 flex justify-center items-center">
+        <button className="py-2 px-12 border border-gray-500/20 text-gray-500/80 font-normal cursor-pointer">See more</button>
+      </div>
+    </section>
+  );
+};
+
+export default HomePopularproduct;
--- src/features/shop/components/Shop.tsx
+++ src/features/shop/components/Shop.tsx
@@ -1,40 +1,45 @@
 import Image, { StaticImageData } from "next/image";
 import { Heart } from "lucide-react";
-import star from '../../../../public/assets/star_icon.svg'
-import apple_earphon from '../../../../public/assets/apple_earphone_image.png'
-import heart from '../../../../public/assets/heart_icon.svg'
+import star from "../../../../public/assets/star_icon.svg";
+import heart from "../../../../public/assets/heart_icon.svg";
 
 type CardProps = {
-    image: StaticImageData;
-    title: string;
-    description: string;
-    price: number;
-    rating: number;
-    key : number;
-}
+  image: StaticImageData;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+};
 
-export default function Card({image, title, description, price, rating, key}: CardProps) {
+export default function Card({
+  image,
+  title,
+  description,
+  price,
+  rating,
+}: CardProps) {
   return (
-    <div className="sm:w-fit w-full rounded-xl bg-white p-4 shadow-sm hover:shadow-md transition mx-auto flex flex-col justify-start" key={key}>
+    <div className="sm:w-fit w-full rounded-xl bg-white p-4 shadow-sm hover:shadow-md transition mx-auto flex flex-col justify-start">
       <div className="relative rounded-lg sm:h-52 max-h-40 bg-gray-100 p-4 flex justify-center">
         <button className="absolute top-2 right-2 rounded-full bg-white p-1 shadow">
           <Image
-          src={heart}
-          alt="heart"
-          height={12}
-          width={12}
-          className="cursor-pointer hover:scale-110"
-           />
+            src={heart}
+            alt="heart"
+            height={12}
+            width={12}
+            className="cursor-pointer hover:scale-110"
+          />
         </button>
 
         <Image
           src={image}
-          alt="Bose QuietComfort 45"
+          alt={title}
           width={160}
           height={160}
           className="object-contain sm:w-40 sm:h-40 w-30 h-30 hover:scale-105 transition-transform duration-300 cursor-pointer"
         />
       </div>
+
       <div className="mt-2 flex flex-col gap-1">
         <h3 className="sm:text-sm text-[14px] font-semibold text-gray-700">
           {title}
@@ -43,9 +48,10 @@ export default function Card({image, title, description, price, rating, key}: Ca
         <p className="sm:text-xs text-[10px] text-gray-400">
           {description}
         </p>
+
         <div className="flex items-center">
           {Array.from({ length: 5 }).map((_, i) => (
-            <Image  
+            <Image
               key={i}
               src={star}
               alt="Star"
@@ -55,11 +61,16 @@ export default function Card({image, title, description, price, rating, key}: Ca
           ))}
           <span className="text-xs text-gray-500 ml-1">{rating}</span>
         </div>
-        <div className="flex items-center justify-between mt-1">
-          <span className="font-semibold text-gray-700 text-sm tracking-wide">{price}</span>
 
-          <button className="rounded-full border px-4 py-1.5 text-xs font-medium
-            hover:bg-orange-600 hover:text-white cursor-pointer text-gray-500 transition whitespace-nowrap">
+        <div className="flex items-center justify-between mt-1">
+          <span className="font-semibold text-gray-700 text-sm tracking-wide">
+            {price}
+          </span>
+
+          <button
+            className="rounded-full border px-4 py-1.5 text-xs font-medium
+            hover:bg-orange-600 hover:text-white cursor-pointer text-gray-500 transition whitespace-nowrap"
+          >
             Buy now
           </button>
         </div>
